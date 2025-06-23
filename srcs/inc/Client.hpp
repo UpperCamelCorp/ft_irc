931548c30efd813d2915731eac6dafd2795c353b
Mickael Chen <michen@f1r11s13.clusters.42paris.fr>
2025-06-23 13:13:02 +0200

    OK

--- srcs/inc/Client.hpp
+++ srcs/inc/Client.hpp
@@ -1,24 +1,24 @@
 #ifndef CLIENT_HPP
 # define CLIENT_HPP
 
-# include <iostream>
-# include <string>
-# include <cstring>
 # include <cstdlib>
-# include <vector>
-# include <sstream>
-#include <map>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <poll.h>
-#include <signal.h>
-#include <unistd.h>
+# include <cstring>
 # include <fcntl.h>
-#include <sstream>
-#include <string>
+# include <iostream>
+# include <map>
+# include <netinet/in.h>
+# include <poll.h>
+# include <signal.h>
+# include <sstream>
+# include <sstream>
+# include <string>
+# include <string>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <vector>
 
-class Server;
+class		Server;
 
 typedef struct s_authenticationStep
 {
@@ -26,36 +26,36 @@ typedef struct s_authenticationStep
 	bool	isUserSet;
 	bool	isRegistered;
 	bool	isPasswordSet;
-} t_authStep;
-
+}			t_authStep;
 
 class Client
 {
-	private:
-		Server      *_server; // pointer vers le l'intsance de Server (pour les channels)
-		int         _socket_fd;
-		std::string _username;
-		std::string _hostname;
-		std::string _servername;
-		std::string _realname;
-		std::string _nickname;
-		t_authStep  _authStep;
-		std::string _recvCommand;
-		void		ircCommand(std::string command);
-		void		nickCommand(std::string command);
-		void		userCommand(std::string command);
-		void		pingCommand(std::string command);
-		void		quitCommand(std::string command);
-		void		unavailableCommand(std::string command);
-		void		authClient();
-	public:
-		Client();
-		void		setServer(Server &server);
-		void		setSocketFd(int fd);
-		int			getSocketFd() const;
-		void		handleCommand(std::string command);
+  private:
+	Server *_server; // pointer vers le l'intsance de Server (pour les channels)
+	int _socket_fd;
+	std::string _username;
+	std::string _hostname;
+	std::string _servername;
+	std::string _realname;
+	std::string _nickname;
+	t_authStep _authStep;
+	std::string _recvCommand;
+	void ircCommand(std::string command);
+	void nickCommand(std::string command);
+	void userCommand(std::string command);
+	void pingCommand(std::string command);
+	void quitCommand(std::string command);
+	void unavailableCommand(std::string command);
+	void authClient();
 
-        std::string getNick();
+  public:
+	Client();
+	void setServer(Server &server);
+	void setSocketFd(int fd);
+	int getSocketFd() const;
+	void handleCommand(std::string command);
+
+	std::string getNick();
 };
 
 #endif
