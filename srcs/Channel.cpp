4e579ef892742a74746074d39cdca5035a22456e
Arasaka <109981070+ThisIsArasaka@users.noreply.github.com>
2025-07-14 15:41:33 +0200

    Update Channel.cpp

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -315,6 +315,7 @@ std::string Channel::getMode() const {
     if (this->_maxClients > 0)
         mode += "l";
     return mode;
+}
 
 bool Channel::isClientInvited(const Client &client) const {
     for (std::vector<Client>::const_iterator it = this->_invitedClients.begin(); it != this->_invitedClients.end(); ++it) {
